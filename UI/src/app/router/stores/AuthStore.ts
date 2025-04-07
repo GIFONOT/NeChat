14e941f6ade4e28e40ee9c01f17d3f9ca44fd088
Poloznikov Artem <art-clon@mail.ru>
2025-04-08 00:47:46 +0500

    Рабочий вариант

--- UI/src/app/router/stores/AuthStore.ts
+++ UI/src/app/router/stores/AuthStore.ts
@@ -1,4 +1,5 @@
 import { defineStore } from "pinia";
+import { useUserStore } from "@stores/UserStore";
 import apiClient from "@/api";
 
 interface User {
@@ -20,6 +21,7 @@ export const useAuthStore = defineStore("auth", {
     async login(email: string, password: string) {
       try {
         const response = await apiClient.post("/auth/login", { email, password });
+        const userStore = useUserStore();
 
         if (!response.data.access_token) {
           throw new Error("Ответ сервера не содержит токен!");
@@ -35,6 +37,9 @@ export const useAuthStore = defineStore("auth", {
 
         localStorage.setItem("token", this.token);
         localStorage.setItem("user", JSON.stringify(this.user));
+
+        userStore.updateUsername(response.data.username);
+        userStore.updateImage("https://sun9-11.userapi.com/impg/tPC_WVw9-lSqlypnpBxySZm9eloqJBL9di2tSQ/j6onL53z90o.jpg?size=456x492&quality=95&sign=49455024b494d4189109706212579dfe&type=album");
 
         console.log("Успешный вход:", this.user);
       } catch (error) {
--- UI/src/app/router/stores/UserStore.ts
+++ UI/src/app/router/stores/UserStore.ts
@@ -13,21 +13,10 @@ export const useUserStore = defineStore("user", {
     user: localStorage.getItem("user") 
       ? JSON.parse(localStorage.getItem("user") as string) 
       : null as User | null,
-    // user: {
-    //   id: 1,
-    //   email: "ame@example.com",
-    //   username: userStorage.username,
-    //   image: "https://sun9-11.userapi.com/impg/tPC_WVw9-lSqlypnpBxySZm9eloqJBL9di2tSQ/j6onL53z90o.jpg?size=456x492&quality=95&sign=49455024b494d4189109706212579dfe&type=album",
-    // } as User,
     mic: true,
   }),
 
   actions: {
-    setUser(newUser: User) {
-      this.user = newUser;
-      localStorage.setItem("user", JSON.stringify(newUser));
-    },
-  
     updateUsername(username: string) {
       if (this.user) {
         this.user.username = username;
